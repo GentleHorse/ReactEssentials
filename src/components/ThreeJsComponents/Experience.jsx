f1874716189cb7a670cba9415bcbdc3a4f8f4411
GentleHorse <anch.abc.t@gmail.com>
2024-05-11 23:22:13 +0200

    Update the three js background scene

--- src/components/ThreeJsComponents/Experience.jsx
+++ src/components/ThreeJsComponents/Experience.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import * as THREE from "three";
 import { useFrame, extend } from "@react-three/fiber";
 import {
@@ -6,8 +6,14 @@ import {
   OrbitControls,
   shaderMaterial,
   useGLTF,
+  Environment,
+  Lightformer,
+  useMatcapTexture,
+  Center,
+  Text3D,
 } from "@react-three/drei";
 import { Perf } from "r3f-perf";
+import { useControls, button } from "leva";
 import vertexShader from "./shaders/plane/vertex.glsl";
 import fragmentShader from "./shaders/plane/fragment.glsl";
 
@@ -23,13 +29,48 @@ const PlaneMaterial = shaderMaterial(
 
 extend({ PlaneMaterial });
 
+const torusGeometry = new THREE.TorusGeometry(1, 0.6, 16, 32);
+const material = new THREE.MeshMatcapMaterial();
+
 export default function Experience() {
+  /**
+   * REFS
+   */
   const planeRef = useRef();
   const planeMaterialRef = useRef();
   const reactIconRef = useRef();
 
+  /**
+   * REACT ICON MODEL
+   */
   const { nodes } = useGLTF("/models/react-icon.glb");
 
+  /**
+   * TRANSMISSION MATERIAL CONFIG
+   */
+  // const { ...config } = useControls({
+  //   backside: true,
+  //   backsideThickness: { value: 0.3, min: 0, max: 2 },
+  //   samples: { value: 16, min: 1, max: 32, step: 1 },
+  //   resolution: { value: 1024, min: 64, max: 2048, step: 64 },
+  //   transmission: { value: 1, min: 0, max: 1 },
+  //   clearcoat: { value: 0, min: 0.1, max: 1 },
+  //   clearcoatRoughness: { value: 0.0, min: 0, max: 1 },
+  //   thickness: { value: 0.3, min: 0, max: 5 },
+  //   chromaticAberration: { value: 5, min: 0, max: 5 },
+  //   anisotropy: { value: 0.3, min: 0, max: 1, step: 0.01 },
+  //   roughness: { value: 0, min: 0, max: 1, step: 0.01 },
+  //   distortion: { value: 0.5, min: 0, max: 4, step: 0.01 },
+  //   distortionScale: { value: 0.1, min: 0.01, max: 1, step: 0.01 },
+  //   temporalDistortion: { value: 0, min: 0, max: 1, step: 0.01 },
+  //   ior: { value: 1.5, min: 0, max: 2, step: 0.01 },
+  //   color: "#ff9cf5",
+  //   gColor: "#ff7eb3",
+  // });
+
+  /**
+   * PLANE ANIMATION
+   */
   useFrame(
     (state, delta) =>
       (planeRef.current.rotation.y =
@@ -40,14 +81,35 @@ export default function Experience() {
       (planeMaterialRef.current.uTime = state.clock.getElapsedTime())
   );
 
+  /**
+   * REACT ICON ANIMATION
+   */
+  // useFrame((state, delta) => {
+  //   reactIconRef.current.rotation.x += delta * 0.1;
+  //   reactIconRef.current.rotation.y += delta * 0.15;
+  // });
+
+  const donutsRef = useRef([]);
+
+  const [matcapTexture] = useMatcapTexture("1A2461_3D70DB_2C3C8F_2C6CAC", 256);
+
+  useEffect(() => {
+    matcapTexture.colorSpace = THREE.SRGBColorSpace;
+    matcapTexture.needsUpdate = true;
+
+    material.matcap = matcapTexture;
+    material.needsUpdate = true;
+  }, []);
+
   useFrame((state, delta) => {
-    reactIconRef.current.rotation.x += delta * 0.10;
-    reactIconRef.current.rotation.y += delta * 0.15;
-  })
+    for (const donut of donutsRef.current) {
+      donut.rotation.y += delta * 0.1;
+    }
+  });
 
   return (
     <>
-      <color args={["#030202"]} attach="background" />
+      <color args={["snow"]} attach="background" />
 
       {/* <Perf position="top-left" /> */}
 
@@ -56,7 +118,17 @@ export default function Experience() {
       <directionalLight position={[1, 2, -2]} intensity={4.5} />
       <ambientLight intensity={1.5} />
 
-      <mesh
+      {/* <Environment resolution={32}>
+        <group rotation={[-Math.PI / 4, -0.3, 0]}>
+          <Lightformer intensity={20} rotation-x={Math.PI / 2} position={[0, 5, -9]} scale={[10, 10, 1]} />
+          <Lightformer intensity={2} rotation-y={Math.PI / 2} position={[-5, 1, -1]} scale={[10, 2, 1]} />
+          <Lightformer intensity={2} rotation-y={Math.PI / 2} position={[-5, -1, -1]} scale={[10, 2, 1]} />
+          <Lightformer intensity={2} rotation-y={-Math.PI / 2} position={[10, 1, 0]} scale={[20, 2, 1]} />
+          <Lightformer type="ring" intensity={2} rotation-y={Math.PI / 2} position={[-0.1, -1, -5]} scale={10} />
+        </group>
+      </Environment> */}
+
+      {/* <mesh
         ref={reactIconRef}
         geometry={nodes.reactIcon.geometry}
         scale={30}
@@ -73,9 +145,9 @@ export default function Experience() {
           distortion={0.5}
           resolution={512}
         />
-      </mesh>
+      </mesh> */}
 
-      <mesh scale={0.9}>
+      {/* <mesh scale={0.9}>
         <sphereGeometry args={[1, 16, 8]} />
         <MeshTransmissionMaterial
           color="lightsteelblue"
@@ -87,12 +159,111 @@ export default function Experience() {
           distortion={0.5}
           resolution={512}
         />
-      </mesh>
+      </mesh> */}
 
       <mesh ref={planeRef} position={[0, 0, -1.5]} scale={15}>
         <planeGeometry args={[2, 1]} />
         <planeMaterial ref={planeMaterialRef} />
       </mesh>
+
+      {/* ------------------ */}
+
+      <group position={[0, 0, 3]}>
+        <Center>
+          <Text3D
+          position={[0, 0.7, 0]}
+            font="./fonts/helvetiker_regular.typeface.json"
+            size={1.2}
+            height={0.2}
+            curveSegments={12}
+            bevelEnabled
+            bevelThickness={0.02}
+            bevelSize={0.02}
+            bevelOffset={0}
+            bevelSegments={5}
+          >
+            REACT
+            <MeshTransmissionMaterial
+              color="lightsteelblue"
+              transmissionSampler
+              transmission={1.2}
+              thickness={0.3}
+              backsideThickness={0.2}
+              chromaticAberration={0.1}
+              distortion={0.5}
+              resolution={512}
+            />
+          </Text3D>
+          <Text3D
+          position={[0, -0.7, 0]}
+            font="./fonts/helvetiker_regular.typeface.json"
+            size={1.2}
+            height={0.2}
+            curveSegments={12}
+            bevelEnabled
+            bevelThickness={0.02}
+            bevelSize={0.02}
+            bevelOffset={0}
+            bevelSegments={5}
+          >
+            ESSENTIALS
+            <MeshTransmissionMaterial
+              color="lightsteelblue"
+              transmissionSampler
+              transmission={1.2}
+              thickness={0.3}
+              backsideThickness={0.2}
+              chromaticAberration={0.1}
+              distortion={0.5}
+              resolution={512}
+            />
+          </Text3D>
+        </Center>
+      </group>
+
+      {[...Array(100)].map((value, index) => (
+        <group
+          key={index}
+          position={[
+            (Math.random() - 0.5) * 20,
+            (Math.random() - 0.5) * 10,
+            (Math.random()) * 10,
+          ]}
+          scale={3 + Math.random() * 2}
+          rotation={[Math.random() * Math.PI, Math.random() * Math.PI, 0]}
+        >
+          <mesh
+            ref={(element) => (donutsRef.current[index] = element)}
+            // geometry={torusGeometry}
+            geometry={nodes.reactIcon.geometry}
+            material={material}
+          >
+            {/* <MeshTransmissionMaterial
+              color="lightsteelblue"
+              transmissionSampler
+              transmission={1.2}
+              thickness={0.3}
+              backsideThickness={0.2}
+              chromaticAberration={0.1}
+              distortion={0.5}
+              resolution={512}
+            /> */}
+          </mesh>
+          <mesh scale={0.03} material={material}>
+            <sphereGeometry args={[1, 16, 8]} />
+            {/* <MeshTransmissionMaterial
+              color="lightsteelblue"
+              transmissionSampler
+              transmission={1.2}
+              thickness={0.3}
+              backsideThickness={0.2}
+              chromaticAberration={0.1}
+              distortion={0.5}
+              resolution={512}
+            /> */}
+          </mesh>
+        </group>
+      ))}
     </>
   );
 }
